a8aaec9a8ddb4a897e881a2338eab2bcfffaa3c9
Joseph Lynn <josephlynn94@gmail.com>
2021-08-10 05:54:13 -0400

    added react router and basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
 import 'normalize.css';
 import './index.css';
 
 function App() {
-  return <div>App Placeholder</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/search">
+          <SearchPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
