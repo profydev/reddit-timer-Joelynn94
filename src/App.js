95554785c37025fe7e84a5b3d00e11b0744583ed
Joseph Lynn <josephlynn94@gmail.com>
2021-08-10 05:43:43 -0400

    added css reset and global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
+import 'normalize.css';
+import './index.css';
 
 function App() {
-  return (
-    <div>
-      App Placeholder
-    </div>
-  );
+  return <div>App Placeholder</div>;
 }
 
 export default App;
