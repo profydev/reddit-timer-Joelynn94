f907cdd3e24dba193cc7cb86dfa56bc60cbe6a22
Joseph Lynn <josephlynn94@gmail.com>
2021-08-10 11:59:12 -0400

    Create app skeleton (#1)
    * added google fonts
    
    * added css reset and global styles
    
    * added react router and basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import 'normalize.css';
+import './index.css';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/search">
+          <SearchPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
