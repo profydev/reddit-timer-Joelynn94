94fc745707a0f8c846f5c1daec45392bbdf27cf2
Joseph Lynn <josephlynn94@gmail.com>
2021-08-10 06:06:56 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
+import Header from './components/Header';
 import 'normalize.css';
 import './index.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/">
           <HomePage />
