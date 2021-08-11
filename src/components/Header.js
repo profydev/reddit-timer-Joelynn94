ad0f31c1178db9b6faf5f70bed4293f7de89e0a5
Joseph Lynn <josephlynn94@gmail.com>
2021-08-11 14:21:12 -0400

    finished stlying the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import logo from './logo.png';
+import './Header.css';
 
 function Header() {
   return (
-    <header>
+    <header className="header">
       <div className="logo">
         <Link to="/">
           <img src={logo} alt="reddit timer logo" />
         </Link>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/search">Search</Link>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <Link to="/search" className="nav__link">
+              Search
+            </Link>
           </li>
-          <li>
-            <HashLink to="/#how-it-works">How it works</HashLink>
+          <li className="nav__item">
+            <HashLink to="/#how-it-works" className="nav__link">
+              How it works
+            </HashLink>
           </li>
-          <li>
-            <HashLink to="/#about">About</HashLink>
+          <li className="nav__item">
+            <HashLink to="/#about" className="nav__link">
+              About
+            </HashLink>
           </li>
         </ul>
       </nav>
