9da0c435381f642b2edb5f32becff7bc724dd736
Joseph Lynn <josephlynn94@gmail.com>
2021-08-11 13:40:32 -0400

    added logo and nav to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+import logo from './logo.png';
 
 function Header() {
-  return <header>Header Placeholder</header>;
+  return (
+    <header>
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="reddit timer logo" />
+        </Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <HashLink to="/#how-it-works">How it works</HashLink>
+          </li>
+          <li>
+            <HashLink to="/#about">About</HashLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
